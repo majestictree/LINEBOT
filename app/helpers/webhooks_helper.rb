65234b7762bfa54f6a83aa54d64367f2809f5ad9
UkiAbe <unlimited.blade.works22@gmail.com>
2019-01-26 22:39:46 +0900

    テスト

--- app/helpers/webhooks_helper.rb
+++ app/helpers/webhooks_helper.rb
@@ -24,7 +24,7 @@ module WebhooksHelper
     return :burnable_garbage     if burnable_garbage?
     return :recyclable_garbage   if recyclable_garbage?
     return :bottle_can_garbage   if bottle_can_garbage?
-    return :not_burnable_garbage if not_burnable_garbage?
+    # return :not_burnable_garbage if not_burnable_garbage?
     :none
   end
 
@@ -33,7 +33,7 @@ module WebhooksHelper
     when :burnable_garbage     then '燃えるゴミの日です'
     when :recyclable_garbage   then '資源ごみ(古紙)の日です'
     when :bottle_can_garbage   then '缶・ビンの日です'
-    when :not_burnable_garbage then '不燃ごみの日です'
+    # when :not_burnable_garbage then '不燃ごみの日です'
     else '特に出せるゴミはありません'
     end
     "明日(#{target_day.strftime("%m月%d日 %a")})は、#{message}"
