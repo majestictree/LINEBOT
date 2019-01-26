33ac10aba7c8744e970f7fd8052280f90efdb659
UkiAbe <unlimited.blade.works22@gmail.com>
2019-01-26 22:35:54 +0900

    ゴミの日を計算する機能追加

--- app/helpers/webhooks_helper.rb
+++ app/helpers/webhooks_helper.rb
@@ -1,41 +1,41 @@
 module WebhooksHelper
-  # def target_day
-  #   @target_day ||= Date.today
+  def target_day
+    @target_day ||= Date.today
+  end
+
+  def burnable_garbage?
+    target_day.tuesday? || target_day.friday?
+  end
+
+  def recyclable_garbage?
+    target_day.monday?
+  end
+
+  def bottle_can_garbage?
+    target_day.monday?
+  end
+
+  # def not_burnable_garbage?
+  #   now = target_day
+  #   now.saturday? && now.mweek == 2
   # end
 
-  # def burnable_garbage?
-  #   target_day.tuesday? || target_day.friday?
-  # end
-
-  # def recyclable_garbage?
-  #   target_day.monday?
-  # end
-
-  # def bottle_can_garbage?
-  #   target_day.monday?
-  # end
-
-  # # def not_burnable_garbage?
-  # #   now = target_day
-  # #   now.saturday? && now.mweek == 2
-  # # end
-
-  # def check
-  #   return :burnable_garbage     if burnable_garbage?
-  #   return :recyclable_garbage   if recyclable_garbage?
-  #   return :bottle_can_garbage   if bottle_can_garbage?
-  #   return :not_burnable_garbage if not_burnable_garbage?
-  #   :none
-  # end
+  def check
+    return :burnable_garbage     if burnable_garbage?
+    return :recyclable_garbage   if recyclable_garbage?
+    return :bottle_can_garbage   if bottle_can_garbage?
+    return :not_burnable_garbage if not_burnable_garbage?
+    :none
+  end
 
   def notice_message
-  #   message = case check
-  #   when :burnable_garbage     then '燃えるゴミの日です'
-  #   when :recyclable_garbage   then '資源ごみ(古紙)の日です'
-  #   when :bottle_can_garbage   then '缶・ビンの日です'
-  #   when :not_burnable_garbage then '不燃ごみの日です'
-  #   else '特に出せるゴミはありません'
-  #   end
-    "明日は燃える日だ"
+    message = case check
+    when :burnable_garbage     then '燃えるゴミの日です'
+    when :recyclable_garbage   then '資源ごみ(古紙)の日です'
+    when :bottle_can_garbage   then '缶・ビンの日です'
+    when :not_burnable_garbage then '不燃ごみの日です'
+    else '特に出せるゴミはありません'
+    end
+    "明日(#{target_day.strftime("%m月%d日 %a")})は、#{message}"
   end
 end
